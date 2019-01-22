0b4283d6490756781997059aaea9ae84cc881203
Carent-Exia <corentin.smigiel@viacesi.fr>
2019-01-22 17:03:00 +0100

    beautify file

--- resources/views/ExternalAPI.blade.php
+++ resources/views/ExternalAPI.blade.php
@@ -1,13 +1,15 @@
 <!doctype html>
 <html>
+
 <head>
-<title>API National du CESI</title>
+    <title>API National du CESI</title>
 </head>
+
 <body>
-  <!-- form serves to receive auth token -->
+    <!-- form serves to receive auth token -->
     <h1>Sign in to external Server</h1>
-    <form id="frm" name="myForm" onsubmit = "return(validate());" method="post">
-      {{ csrf_field() }}
+    <form id="frm" name="myForm" onsubmit="return(validate());" method="post">
+        {{ csrf_field() }}
         <div>
             <input type="test" name="Name" placeholder="Ville de votre centre">
         </div>
@@ -17,54 +19,55 @@
         </div>
 
         <div>
-            <input id="submit" type="button" value="Submit"/>
+            <input id="submit" type="button" value="Submit" />
         </div>
     </form>
 
     <!-- form to access API -->
     <script src="/js/jquery-3.3.1.js"></script>
     <script src="/js/cookie.js"></script>
-    <script type = "text/javascript">
-    $(document).ready(function() {
-      //var onready = listCookies();
-      //console.log(onready);
-      //alert(onready);
-      //form validation
-      $("#submit").on('click', function () {
-        if (document.myForm.Name.value == "") {
-          alert("please provide your name!");
-          document.myForm.Name.focus();
-          return false;
-        }
-        if (document.myForm.Password.value == "") {
-          alert("Password field is empty!");
-          document.myForm.Password.focus();
-          return false;
-        }
-        //send data to API
-        $.ajax({
-          url:'http://localhost:3000/auth/signin',
-          type : 'POST',
-          dataType : 'json',
-          //contentType: "application/json",
-            data: $('#frm').serializeArray(),
-            success : function(json) {
-          //    alert("success");
-            console.log("success");
-            var Jtoken = json.token;
-        //    console.log(Jtoken);
-            setCookie("token",Jtoken, 7);
-            var test = listCookies();
-            console.log(test);
-          },
-          error : function(xhr,resp, text) {
-            console.log(xhr, resp, text);
-            console.log('error');
-          }
-        })
-      })
-    });
+    <script type="text/javascript">
+        $(document).ready(function() {
+            //var onready = listCookies();
+            //console.log(onready);
+            //alert(onready);
+            //form validation
+            $("#submit").on('click', function() {
+                if (document.myForm.Name.value == "") {
+                    alert("please provide your name!");
+                    document.myForm.Name.focus();
+                    return false;
+                }
+                if (document.myForm.Password.value == "") {
+                    alert("Password field is empty!");
+                    document.myForm.Password.focus();
+                    return false;
+                }
+                //send data to API
+                $.ajax({
+                    url: 'http://localhost:3000/auth/signin',
+                    type: 'POST',
+                    dataType: 'json',
+                    //contentType: "application/json",
+                    data: $('#frm').serializeArray(),
+                    success: function(json) {
+                        //    alert("success");
+                        console.log("success");
+                        var Jtoken = json.token;
+                        //    console.log(Jtoken);
+                        setCookie("token", Jtoken, 7);
+                        var test = listCookies();
+                        console.log(test);
+                    },
+                    error: function(xhr, resp, text) {
+                        console.log(xhr, resp, text);
+                        console.log('error');
+                    }
+                })
+            })
+        });
     </script>
 
 </body>
+
 </html>
