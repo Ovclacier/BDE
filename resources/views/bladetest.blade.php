dad7d85df9d128da2c440b9ab7ce8dbb82bcb5b8
Carent-Exia <corentin.smigiel@viacesi.fr>
2019-01-22 17:16:21 +0100

    Add files from branch Front-end

---
+++ resources/views/bladetest.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+@include('includes.head')
+</head>
+<body>
+    @include('includes.header')
+    <br>contenu1<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+    <br>contenu<br>
+
+    @include('includes.footer')
+</body>
+</html>
---
+++ resources/views/jsp.blade.php
@@ -0,0 +1,38 @@
+@extends('HéritageTest')
+
+@section('contenu')
+  <p>
+  <br>blblblb 1<br>
+  <br>blblblb 2<br>
+  <br>blblblb 3<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb 4<br>
+  <br>blblblb end<br>
+  </p>
+@endsection
