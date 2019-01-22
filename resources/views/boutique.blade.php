95b6f9d1ffcf42d42cca5a668054e3b22d2e2db9
Carent-Exia <corentin.smigiel@viacesi.fr>
2019-01-22 10:41:33 +0100

    Add files from branch Front-End

---
+++ resources/views/boutique.blade.php
@@ -0,0 +1,79 @@
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="Content-Language" content="fr" />
+		<title>BDE Boutique</title>
+		<link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+	 	<link rel="stylesheet" href="{{ asset('/css/bootstrap-theme.min.css') }}">
+	 	<link rel="stylesheet" href="{{ asset('/css/bootstrap.css') }}">
+		<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="assets\js\bootstrap.min.js"></script>
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	</head>
+
+	<body>
+
+
+
+
+		<div class = "container-fluid container blc text-center">
+			<h1>Les produits du moments</h1>
+			<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://www.google.fr"><img src="img/pile.png" alt="pile"></a>
+				<div>Pile AA</div>
+			</div>
+			<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://www.google.fr"><img src="img/pile.png" alt="pile"></a>
+				<div>Pile AAA</div>
+			</div>
+			<div class="col-lg-4 col-md-12 col-sm-12">
+				<a href="http://www.google.fr"><img src="img/pile.png" alt="pile"></a>
+				<div>Pile AAAAAAAAAAAA</div>
+			</div>
+		</div>
+
+		<div class="container-fluid container blc text-center ">
+		
+			<div class="row ble1">
+				<h2>Liste produit</h2>
+				</div>
+			<div class="row">
+				<div class="col-lg-3 col-mg-12 col-sm-12 ble2 text-left marginMenu fontSizeMenu">
+					<h3 class="text-center">Coucou</h3>
+					<div>Catégories :</div>
+					<ul>
+						<li>Pile</li>
+						<li>Paire de pile</li>
+						<li>Pile de rechange</li>
+						<li>Pile de montre</li>
+					</ul>
+				</div>
+			
+				<div class="col-lg-9 col-mg-12 col-sm-12 blc">
+					<div class="col-lg-3 col-md-6 col-sm-6">
+						<a href="http://www.google.fr"><img src="img/pile.png" width="200" height="200" alt="pile"></a>
+						<div>Pile AA</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6">
+						<a href="http://www.google.fr"><img src="img/pile.png" width="200" height="200" alt="pile"></a>
+						<div>Pile AAA</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6">
+						<a href="http://www.google.fr"><img src="img/pile.png" width="200" height="200" alt="pile"></a>
+						<div>Pile AAAAAAAAAAAA</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6">
+						<a href="http://www.google.fr"><img src="img/pile.png" width="200" height="200" alt="pile"></a>
+						<div>Pile B</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+				
+	</body>
+	<footer>
+ 		<p><a class="lien" href="http://google.com">Mentions légales</a><a class="barre"> | </a><a>BDE@viacesi.fr</a></p>
+	</footer>
