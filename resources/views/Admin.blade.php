8de4f15af410267b849cc68c59e712e37c3cc224
Ovclacier <33619305+Ovclacier@users.noreply.github.com>
2019-01-23 11:39:45 +0100

    commit

--- resources/views/Admin.blade.php
+++ resources/views/Admin.blade.php
@@ -10,21 +10,35 @@
 	<div>
 		<div class = "container-fluid container blc text-center">
 			<div class="row ble1 ">
-				<h1>Fiche Evenement</h1>
+				<h1 class="headed">Fiche Evenement</h1>
             </div>
             <div class="row ble2 ">
             <div class="col-lg-6 col-md-6 col-sm-12">
-            <button type="button">Click Me!</button>
+            <button type="button">Download to .csv</button>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-12">
-            <button type="button">Click Me!</button>
+            <button type="button">Download to .pdf</button>
             </div>
 			</div>
 			<div class="col-lg-12 col-md-12 col-sm-12">
             <table class="adm"border=1 width=100% cellspacing=1 cellpadding=1 >
+            <?php
+            $nombre_de_lignes = 1;
+
+            while ($nombre_de_lignes <= 100)
+            {
+             echo '<tr><th>'.$nombre_de_lignes;
+             echo '</th><td>'.$nombre_de_lignes;
+             echo '</td><td>'.$nombre_de_lignes;
+             echo '</td><td>'.$nombre_de_lignes;
+             echo '</td><td>'.$nombre_de_lignes.'</td></tr>';
+             $nombre_de_lignes++;
+}
+?>
             <tr><th>Fuck it</th><td>cellule1</td><td>cellule 2</td><td>cellule1</td><td>cellule1</td></tr>
             <tr><th>Cellules vides</th><td></td><td></td></tr>
-            <tr><th>cellules du bas</th><td></td><td>cellule 6</td></tr></table>
+            <tr><th>cellules du bas</th><td></td><td>cellule 6</td></tr>
+            </table>
 
 </table>
 
