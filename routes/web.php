a59be758d6eb62583febd1d1ff308cdbcd54b912
Carent-Exia <corentin.smigiel@viacesi.fr>
2019-01-23 10:01:29 +0100

    Added Routes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+
+//Le login marche, mais la route qui gère /login et /register est où?
+
+/*
+ProductController-related routes
+*/
+Route::get('/create','ProductController@create');
+Route::get('/products','ProductController@index');
+Route::resource('/products', 'ProductController');
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,14 +28,26 @@ Route::get('/', function () {
 
 Route::get('/Evenements', function () {
     return view('event');
+/*
+HomeController-related routes
+*/
+Route::get('/send/email', 'HomeController@mail');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('A', function() {
+  return view('truc');
 });
 
 Route::get('/Boutique', function () {
     return view('boutique');
 });
+/*
+Branch Intermediate
+*/
 
 Route::get('/Connexion', function () {
     return view('truc');
+Route::get('/api', function() {
+  return view('ExternalAPI');
 });
 Route::get('/header', function () {
     return view('header');
@@ -34,6 +57,8 @@ Route::get('/test', function () {
 });
 
 Route::get('/WHAT2', function() {
+//should be restricted to users with appropriate grade
+Route::get('/admin', function() {
   return view('jsp');
 });
 Route::get('/fiche', function() {
