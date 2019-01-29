96c08fb6a875c352cf2d7a61f6bebf490e2ea45d
Axel Mola <axel.mola@viacesi.fr>
2019-01-29 22:57:47 +0100

    some changes (again)
    some changes (again)

--- app/Commande.php
+++ app/Commande.php
@@ -4,7 +4,7 @@ namespace App;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
-class Cart_storage extends Model
+class Commande extends Model
 {
     use SoftDeletes;
     
--- resources/views/shop/cart/cartdetails.blade.php
+++ resources/views/shop/cart/cartdetails.blade.php
@@ -12,7 +12,7 @@
         @foreach ($carts as $cart)
         <tr>
            
-            <td>{{ $cart->cart_data }}</td>
+            <td>{{ $cart->id_produit }}</td>
             <td>{{ $cart->user_id }}</td>
             <td>{{ $cart->quantity }}</td>
             <td>{{ $cart->state }}</td>
