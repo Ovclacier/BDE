115704c3a201e131bacfb59a99bf9001fdf56edc
Ovclacier <33619305+Ovclacier@users.noreply.github.com>
2019-01-29 10:24:30 +0100

    Update connexion.blade.php

--- resources/views/connexion.blade.php
+++ resources/views/connexion.blade.php
@@ -17,40 +17,38 @@
 		<div class = "container-fluid container text-center">
 			<div class="col-lg-1 col-md-0 col-sm-0"></div>
 			<div class="col-lg-4 col-md-12 col-sm-12 wrapper">
-				<form id="Connexion" name="CxForm">
+				<form id="Connexion">
 	                <h2 class="titreEvent">Connexion</h2>
 	                <div class="formContainer">
-	                    <div><input type="text" name="Mail" placeholder="Mail"></div>
-	                    <div><input type="password" name="Mdp" placeholder="Mot de passe"></div>
+	                    <div><input type="text" name="mail" placeholder="Mail"></div>
+	                    <div><input type="password" name="pwd" placeholder="Mot de passe"></div>
 
-	                    <div><br><button id="connect" form="">Connexion</button></div>
+	                    <div><br><button form="Connexion">Connexion</button></div>
 	                </div>
 	            </form>
 	        </div>
 	        <div class="col-lg-1 col-md-0 col-sm-0"></div>
 			<div class="col-lg-4 col-md-12 col-sm-12 wrapper">
-	            <form id="Inscription" name="RegForm" method="post">
+	            <form id="Inscription">
 	                <h2 class="titreEvent">S'inscrire</h2>
 	                <div class="row">
-	                    <div class="col-lg-6 col-md-12 col-sm-12"><input class="logInput blackInput" type="text" name="Mail" placeholder="Mail"></div>
-	                    <div class="col-lg-6 col-md-12 col-sm-12"><input class="logInput blackInput" type="password" name="Mdp" placeholder="Mot de passe"></div>
+	                    <div class="col-lg-6 col-md-12 col-sm-12"><input class="logInput blackInput" type="text" name="mail" placeholder="Mail"></div>
+	                    <div class="col-lg-6 col-md-12 col-sm-12"><input class="logInput blackInput" type="password" name="pwd" placeholder="Mot de passe"></div>
 	                </div>
 	                <div class="row">
-	                    <div class="col-lg-6 col-md-12 col-sm-12"><input class="logInput blackInput" type="text" name="Nom" placeholder="Nom"></div>
-	                    <div class="col-lg-6 col-md-12 col-sm-12"><input class="logInput blackInput" type="text" name="Prenom" placeholder="Prénom"></div>
+	                    <div class="col-lg-6 col-md-12 col-sm-12"><input class="logInput blackInput" type="text" name="nom" placeholder="Nom"></div>
+	                    <div class="col-lg-6 col-md-12 col-sm-12"><input class="logInput blackInput" type="text" name="prénom" placeholder="Prénom"></div>
 	                </div>
 	                <br>
 				    <div class="inForm">Localisation :</div>
-				    <select name="Centre">
+				    <select name="place">
 					    <option>Strasbourg</option>
 					    <option>Paris</option>
 					    <option>Poitou-Charentes</option>
 					    <option>San Fransisco</option>
 					    <option>Tokyo</option>
 				    </select>
-						<p> <input type="checkbox" id="check_form" name="" value="">
-						</p>
-                    <div><br><button id="register" form="">S'inscrire</button></div>
+                    <div><br><button form="Inscription">S'inscrire</button></div>
 	            </form>
 	        </div>
         </div>
@@ -59,93 +57,5 @@
 	<footer class="footerConnexion">
 		<p><a class="lien" href="/BDE/public/mentions">Mentions légales</a><a class="barre"> | </a><a>BDE@viacesi.fr</a></p>
 	</footer>
-
-	<script src="/js/jquery-3.3.1.js"></script>
-		<script src="/js/cookie.js"></script>
-		<script type="text/javascript" src="/js/Jquerycookie/jquery.cookie.js"></script>
-		<script type="text/javascript">
-		$(document).ready(function() {
-
-
-				//form validation
-				$("#register").on('click', function() {
-					console.log($("#check_form").is(":checked"));
-						if (document.RegForm.Mail.value == "" || document.RegForm.Nom.value == "" || document.RegForm.Prenom.value == "" || document.RegForm.Centre.value == "" || document.RegForm.Mdp.value == "") {
-								alert("Veuillez remplir toutes les cases!");
-								document.RegForm.Mail.focus();
-								return false;
-						}
-
-						if ($("#check_form").is(":checked") == false) {
-							alert("please check the box");
-							return false;
-						}
-						//send data to API
-						$.ajax({
-								url: 'http://localhost:3000/api/users',
-								type: 'POST',
-								dataType: 'json',
-								//contentType: "application/json",
-								data: $('#Inscription').serializeArray(),
-								success: function(json) {
-										//    alert("success");
-										//console.log("success");
-										//console.log(json);
-										alert("inscription confirmée. Vous allez être redirigé vers la page d'accueil");
-								//		window.location = "/";
-								},
-								error: function(xhr, resp, text) {
-										console.log(xhr, resp, text);
-										console.log('error');
-								}
-						})
-				})
-		});
-
-		</script>
-		<script src="/js/cookie.js"></script>
-		<script type="text/javascript">
-		$(document).ready(function() {
-				//form validation
-				$("#connect").on('click', function() {
-						if (document.CxForm.Mail.value == "") {
-								alert("Veuillez remplir toutes les cases!");
-								document.CxForm.Mail.focus();
-								return false;
-						}
-						if (document.CxForm.Mdp.value == "") {
-								alert("Veuillez remplir toutes les cases!");
-								document.CxForm.Mdp.focus();
-								return false;
-						}
-						//send user Mail and password to nodeJS API
-						$.ajax({
-							url: 'http://localhost:3000/api/users/login', //API url
-							type: 'POST',
-							dataType: 'json',
-							data: $("#Connexion").serializeArray(), //dara from connexion form
-							success: function(json) {
-								console.log('success');
-								console.log(json);
-								setCookie("UserId", json.data.id, 365);
-								if(json.data.Grade > 1) {
-									//console.log(json.token);
-									//set jsonwebtoken to allow access to API
-									setCookie("token", json.token, 7);
-								} else {
-									eraseCookie("token");
-								}
-								//var test = listCookies();
-								//console.log(test);
-								//alert("Vous êtes bien connectés. Vous allez être redirigé(e).")
-								//window.location = "/";
-
-							}
-						})
-					})
-				});
-
-		</script>
-
 
 </html>
