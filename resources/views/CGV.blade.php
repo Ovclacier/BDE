7de1898accc7c83155c2ab1ce168b7a5f4cc907b
Ovclacier <33619305+Ovclacier@users.noreply.github.com>
2019-01-29 09:25:02 +0100

    Create CGV.blade.php

---
+++ resources/views/CGV.blade.php
@@ -0,0 +1,193 @@
+<p align="center">
+    <strong>Conditions Générales de Vente</strong>
+</p>
+<p>
+    <strong></strong>
+</p>
+<p>
+    Préambule :
+</p>
+<p>
+    L’association Bureau des étudiants cesi Strasbourg propose à la vente des
+    produits destinés à (par exemple : contribuer au financement de son
+    activité). Les offres sont valables dans la limite des stocks disponibles
+    et pour autant qu’elles figurent sur le site. La vente n’est proposée que
+    pour les acheteurs résidants et se faisant livrer sur le territoire
+    métropolitain. En passant commande sur le site, nos clients reconnaissent
+    en avoir pris connaissance et les avoir acceptées.
+</p>
+<p>
+    Données personnelles :
+</p>
+<p>
+    Les données collectées ne sont utilisées que pour pouvoir honorer la
+    commande ; elles sont accessibles par les personnes chargées de la
+    préparation et de l’envoi de la commande. Elles peuvent être transmises aux
+    établissements bancaires pour exécution du paiement. Le responsable du
+    traitement est Romain Koffmann conformément à la Loi informatique et
+    Libertés du 6 janvier 1978, un droit d’accès et de rectification est
+    possible en nous contactant à l’adresse suivante BDE-Strasbourg@cesi.fr
+</p>
+<p>
+    Objet :
+</p>
+<p>
+    Les présentes conditions générales de vente régissent exclusivement les
+    ventes réalisées sur son site internet par l’association [coordonnées
+    complètes de l'association] et dont la présidente est Romain Kaufmann
+</p>
+<p>
+    Produits :
+</p>
+<p>
+    Les produits proposés sont issus d’achats effectués au préalable auprès de
+    commerces et particuliers locaux
+</p>
+<p>
+    Prix :
+</p>
+<p>
+    Les prix sont indiqués en euros TTC hors frais d’envoi, les prix en vigueur
+    au moment de la vente seront ceux appliqué a l’achat du produit.
+</p>
+<p>
+    Les commandes :
+</p>
+<p>
+    Le processus de commande comprend plusieurs étapes :
+</p>
+<p>
+    1) L’internaute sélectionne sur le site les produits qu’il choisit et
+    indique la quantité désirée
+</p>
+<p>
+    2) Un récapitulatif de sa commande lui est proposé et l’internaute a la
+    possibilité de rectifier sa commande
+</p>
+<p>
+    3) L’internaute valide sa commande : en confirmant la commande,
+    l’internaute reconnait son obligation de paiement
+</p>
+<p>
+    4) Une fois la commande validée, l’association Bureaux des Etudiants Cesi
+    Strasbourg envoie dans les meilleurs délais un accusé de réception de la
+    commande.
+</p>
+<p>
+    Paiement :
+</p>
+<p>
+    Le paiement en ligne peut se faire par Paypal; vos informations de paiement
+    ne seront pas conservées après le paiement.
+</p>
+<p>
+    Livraison :
+</p>
+<p>
+    Nous ne pouvons assurer l’envoi des produits proposés. La livraison se fera
+    lors d’un rendez-vous avec un membre de notre équipe à l’endroit et l’heure
+    indiquée dans le contenu d’un e-mail envoyé à l’acheteur lors de sa
+    commande. Dans la mesure du possible, nous procédons à un envoi groupé. Si
+    cela n’est pas possible, les frais de livraison des envois complémentaires
+    sont à la charge de l’association.
+</p>
+<p>
+    Droit de rétractation :
+</p>
+<p>
+    En application de l’article L121-17 I du code de la consommation, vous avez
+    le droit de vous rétracter de la présente vente, sans donner de motif, dans
+    un délai de quatorze jours.
+</p>
+<p>
+    Le délai de rétractation expire quatorze jours après le jour où vous-même,
+    ou un tiers autre que le transporteur et désigné par vous, prend
+    physiquement possession du bien (ou du dernier bien si plusieurs colis
+    issus de la même commande sont nécessaires).
+</p>
+<p>
+    Pour exercer le droit de rétractation, vous devez nous notifier à
+    BDE-Strasbourg@cesi.fr votre décision de rétractation, au moyen d'une
+    déclaration dénuée d'ambiguïté (par exemple, lettre envoyée par la poste,
+    télécopie ou courrier électronique).
+</p>
+<p>
+    Pour que le délai de rétractation soit respecté, il suffit que vous
+    transmettiez votre décision de rétractation avant l'expiration du délai de
+    rétractation.
+</p>
+<p>
+    Effets de rétractation :
+</p>
+<p>
+    En cas de rétractation de votre part, nous vous rembourserons tous les
+    paiements reçus de vous, y compris les frais de livraison.
+</p>
+<p>
+    Ce remboursement sera fait sans retard excessif et au plus tard quatorze
+    jours à compter du jour où nous sommes informés de votre décision de
+    rétractation.
+</p>
+<p>
+    Nous procéderons au remboursement en utilisant le même moyen de paiement
+    que celui que vous aurez utilisé pour la transaction initiale ; en tout
+    état de cause, ce remboursement n'occasionnera pas de frais pour vous. Nous
+    pouvons différer le remboursement jusqu'à ce que nous ayons reçu le bien ou
+    jusqu'à ce que vous ayez fourni une preuve d'expédition du bien, la date
+    retenue étant celle du premier de ces faits
+</p>
+<p>
+    Vous devrez renvoyer ou rendre le bien, à nous-mêmes sans retard excessif,
+    au plus tard quatorze jours après que vous nous aurez communiqué votre
+    décision de rétractation du présent contrat. Ce délai est considéré
+    respecté si vous renvoyez le bien avant l'expiration du délai de quatorze
+    jours. Votre responsabilité n'est engagée qu'à l'égard de la dépréciation
+    du bien résultant de manipulations autres que celles nécessaires pour
+    établir la nature, les caractéristiques et le bon fonctionnement de ce
+    bien.
+</p>
+<p>
+    Vous devrez prendre en charge les frais directs de renvoi du bien.
+</p>
+<p>
+    Garanties :
+</p>
+<p>
+    En application du code de la consommation (article L211-4, L133-3 et
+    R111-1), nous sommes garants de la conformité des produits commandés
+    pendant les 2 ans qui suivent l’achat. Vous devez nous prévenir par
+    courrier et nous envisagerons la réparation, le remplacement ou le
+    remboursement si les deux premières solutions ne peuvent être mises en
+    œuvre
+</p>
+<p>
+    La garantie des vices cachés prévues aux articles 1641 à 1648 et 2232du
+    code civil s’applique également.
+</p>
+<p>
+    Ces garanties s’appliquent sans préjudice du droit de rétractation.
+</p>
+<p>
+    Réclamations :
+</p>
+<p>
+    Vos réclamations concernant votre commande sont à adresser à
+    BDE-Strasbourg@cesi.fr
+</p>
+<p>
+    Vous pouvez également nous téléphoner au
+    <a
+        href="https://www.google.com/search?q=numero+BDE+cesi&amp;rlz=1C1CHBF_frFR821FR821&amp;oq=numero+BDE+cesi&amp;aqs=chrome..69i57.4700j0j7&amp;sourceid=chrome&amp;ie=UTF-8"
+        title="Appeler avec Hangouts"
+    >
+        03 88 10 35 60
+    </a>
+</p>
+<p>
+    Si la réponse que nous vous apportons ne vous satisfait pas, vous pouvez
+    contacter la plateforme de règlement des litiges en ligne mise en place par
+    la commission européenne.
+</p>
+<p>
+    https://webgate.ec.europa.eu/odr/main/index.cfm?event=main.adr.show
+</p>
