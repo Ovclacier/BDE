4cc199a88d5c55f4b537b3ed7875c88b014a3a30
CorentinLarche <corentin.larche@viacesi.fr>
2019-01-30 08:54:22 +0100

    Update createproduct.blade.php

--- resources/views/shop/createproduct.blade.php
+++ resources/views/shop/createproduct.blade.php
@@ -1,26 +1,29 @@
-<!doctype html>
-<html>
+
+@extends('layout')
+
+	@section('title')
+	<title>Ajouter Produit</title>
+	@endsection
+
+	@section('contenu')
 
 
+		<div class="container container-fluid blc text-center">
+			<div class="row ble1 menuTop">
+				<h1>Ajouter un produit</h1>
+			</div>
 
-<body>
-            <form action="/products" method="POST" enctype="multipart/form-data">
-
+            <form action="{{route('produits.store')}}" method="POST" enctype="multipart/form-data">
                 @csrf
-                <h1> Enter Details to create a product</h1>
                
-                    <label>Name</label> <input type="text" name="name">          
+                    <div class="col-lg-3 col-md-3 col-sm-3"><label>Name</label> <input type="text" name="Nom_article"></div>        
 
-                    <label>Image</label><input type="file" name="image" id="image">
+                    <div class="col-lg-3 col-md-3 col-sm-3"><label>Image</label><input type="file" name="URL_image" id="image"></div>
                
-                    <label>Description</label> <input type="text" name="description">
+                    <div class="col-lg-3 col-md-3 col-sm-3"><label>Description</label> <input type="text" name="description"></div>
 
-                    <label>Price</label> <input type="number" name="price">
+                    <div class="col-lg-3 col-md-3 col-sm-3"><label>Price</label> <input type="number" name="prix" step="0.01" min="0"></div>
 
-                <button type="submit">Submit</button>
+                <div class="row marginTop25"><button type="submit">Submit</button></div>
             </form>
-
-      
-</body>
-
-</html>
+		</div>
